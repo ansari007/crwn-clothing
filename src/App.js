9fe0f9f74a55e24d2de59ed46ba11455e568ca89
mohamad.ansari <mohamad.ansari@cleartag.com>
2019-11-06 12:58:59 +0200

    adding shop page and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Route,Switch } from "react-router-dom";
-import HomePage from './pages/homepage/homepage.component';
-
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component'
 import './App.css';
- const HatsPage=(props)=>(
-   <div>
-   <h1>HATS PAGE</h1>
-   </div>
- )
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
 function App() {
- 
-    return (
-      <div>
+  return (
+    <div>
+      <Header/>
       <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
-    </Switch>
-      </div>
-    );
-  
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
